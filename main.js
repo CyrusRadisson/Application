5a83da6571dce0e3a664ab1c89653e976041f54c
PRF <161906307+TopHT24@users.noreply.github.com>
2024-11-21 12:53:12 +0800

    Add files via upload
    created new directory  with these folders to add the Electron property to app

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    
+  })
+
+  
+  mainWindow.loadURL('http://localhost:3000')
+
+
+  
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
